c76d31c327abbbf3a6d5dfbac7d63a79c7e2af3a
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2022-02-07 20:56:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifer = ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
+ function modelLoaded(){
+   console.log("Model Loaded!");
+ }
 
+ function draw(){
+   image(video,0,0,300,300);
+   classifer.classify(video,gotResults);
+ }
 
+ var previous_result = "";
+
+ function gotResults(error,results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    if((results[0].confidence > 0.5) &&(previous_result != results[0].label)){
+      console.log(results);
+      previous_result = results[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = "Object Detected Is" + results[0].label;
+      var utter_This = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utter_This);
+
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+  }
+ }
